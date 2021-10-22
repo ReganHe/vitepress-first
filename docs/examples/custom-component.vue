f707d2a851fe09d4fcbee53e725142e28fde131b
Mark He <heke@hujiang.com>
2021-10-22 11:02:05 +0800

    支持引入 Vue组件

---
+++ docs/examples/custom-component.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="custom-component">
+    This is a Custom Component
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.custom-component {
+  background: rgb(14, 46, 27);
+  color: #ffffff;
+  padding: 12px;
+  font-weight: 16px;
+}
+</style>
